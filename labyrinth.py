c539033115315fcb2a719dd9681ded65dafa5355
vfliagin <93093512+vfliagin@users.noreply.github.com>
2021-10-25 16:19:10 +0300

    Add files via upload
    Several bugs fixed. Visualization was improved.

--- labyrinth.py
+++ labyrinth.py
@@ -2,13 +2,24 @@ import numpy
 
 """Программа создает случайный лабиринт 8х8 и находит из него выход."""
 
+
 class Block:
     def __init__(self, block_type):
-        """Блок содержит информацию о том, за сколько шагов до него можно дойти, ссылку на блок, из которого до него
-        можно дойти, код типа блока (-1 - стена, 0 - пустое пространство, 1 - старт, 2 - финиш)"""
+        """Блок содержит информацию о том, за сколько шагов до него можно дойти, ссылку на блок,
+        из которого до него можно дойти, код типа блока
+        (-1 - стена, 0 - пустое пространство, 1 - старт, 2 - финиш, 3 - путь к выходу)"""
         self.previous_block = None
         self.steps = None
         self.type = block_type
+
+    def print_self(self):
+        """Функция выписывает в консоль тип блока с соответствующей подсветкой."""
+        if self.type == 0:
+            print("+" + str(self.type), end="")
+        elif self.type > 0:
+            print('\x1b[6;30;42m' + "+" + str(self.type) + '\x1b[0m', end="")
+        else:
+            print('\x1b[6;30;41m' + str(self.type) + '\x1b[0m', end="")
 
 
 class Labyrinth:
@@ -31,14 +42,33 @@ class Labyrinth:
     def show_labyrinth(self):
         for x in range(8):
             for y in range(8):
-                print(self.labyrinth_plan[8 * x + y].type, end=" ")
+                self.labyrinth_plan[x * 8 + y].print_self()
+                print("", sep="", end=" ")
             print("")
 
+    @staticmethod
+    def find_the_neighbours(i):
+        res = []
+
+        if i % 8 != 0:
+            res.append(i - 1)
+        if i % 8 != 7:
+            res.append(i + 1)
+        if i + 8 <= 63:
+            res.append(i + 8)
+        if i - 8 >= 0:
+            res.append(i - 8)
+
+        return res
+
     def check_the_neighbours(self, i, to_check):
-        """Функция проверяет соседние с i блоки. Если у блока нет предшественника, он добавляется в очередь, заполняются
-        переменные previous_block и steps. Возвращается выход (если он найден) или None."""
-        for x in [i + 1, i - 1, i + 8, i - 8]:
-            if 0 <= x <= 63 and self.labyrinth_plan[x].type != -1:
+        """Функция проверяет соседние с i блоки. Если у блока нет предшественника, он добавляется в очередь,
+        заполняются переменные previous_block и steps. Возвращается выход (если он найден) или None."""
+
+        neighbours = self.find_the_neighbours(i)
+
+        for x in neighbours:
+            if self.labyrinth_plan[x].type != -1:
                 if self.labyrinth_plan[x].previous_block is None:
                     self.labyrinth_plan[x].previous_block = i
                     self.labyrinth_plan[x].steps = self.labyrinth_plan[i].steps + 1
@@ -48,9 +78,10 @@ class Labyrinth:
         return None
 
     def find_the_exit(self):
-        """Функция ищет выход из лабиринта. Находится стартовый блок. Создается очередь блоков, которые нужно проверить.
-         Изначально это старт, потом его соседи и т.д. Когда очередь становится пустой, либо находится старт, функция
-         выписывает результат (путь от финиша к старту либо сообщение, что пути нет.)."""
+        """Функция ищет выход из лабиринта. Находится стартовый блок. Создается очередь блоков,
+        которые нужно проверить. Изначально это старт, потом его соседи и т.д.
+        Когда очередь становится пустой, либо находится старт,
+        функция выписывает результат (путь от финиша к старту либо сообщение, что пути нет.)."""
         to_check = []
         escape = None
 
@@ -61,23 +92,23 @@ class Labyrinth:
                 self.labyrinth_plan[x].previous_block = x
 
         while to_check and escape is None:
-            current_block = to_check.pop()
+            current_block = to_check.pop(0)
             escape = self.check_the_neighbours(current_block, to_check)
 
         if escape is None:
+            self.show_labyrinth()
             print("No escape.")
             return
 
-        tmp = escape
+        tmp = self.labyrinth_plan[escape].previous_block
         while self.labyrinth_plan[tmp].steps != 0:
-            print(tmp)
+            self.labyrinth_plan[tmp].type = 3
             tmp = self.labyrinth_plan[tmp].previous_block
-        print(tmp)
+        self.show_labyrinth()
 
 
 def main():
     test = Labyrinth()
-    test.show_labyrinth()
     test.find_the_exit()
 
 
